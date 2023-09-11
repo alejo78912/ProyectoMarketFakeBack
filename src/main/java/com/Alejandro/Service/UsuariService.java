7c0c0994526229c7868105378a6f6dee212fcda9
alejo78912 <av739006@gmail.com>
2023-09-11 16:42:44 -0500

    Se arreglaron errores

--- src/main/java/com/Alejandro/Service/UsuariService.java
+++ src/main/java/com/Alejandro/Service/UsuariService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.Alejandro.models.Usuario;
 import com.Alejandro.repository.IUsuarioRepository;
 
+
 @Service
 public class UsuariService {
 
@@ -27,6 +28,24 @@ public class UsuariService {
 	       return usuarioRepository.save(usuario);
 	    }
 	 
+	 
+	 public Usuario obtenerPorId(Long id) {
+	        return usuarioRepository.findById(id).orElse(null);
+	    }
+	 
+
+	    public String autenticarUsuario(String correo, String contrasena) {
+	        Usuario usuario = usuarioRepository.findByEmailUsuario(correo);
+
+	        if (usuario != null && usuario.getConstrasenia().equals(contrasena)) {
+	            // Aquí, puedes implementar la lógica para determinar el tipo de usuario
+	            // basándote en los roles o cualquier otro criterio que tengas en tu sistema.
+	            String tipoUsuario = usuario.getTipoUsuario();
+	            return tipoUsuario;
+	        } else {
+	            return null; // Devolver null si la autenticación falla
+	        }
+	    }
 	 
 	
 }
--- src/main/java/com/Alejandro/controller/demoControllerAuth.java
+++ src/main/java/com/Alejandro/controller/demoControllerAuth.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.Alejandro.Service.UsuariService;
 import com.Alejandro.models.Usuario;
-import com.Alejandro.repository.IUsuarioRepository;
+
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,16 +22,23 @@ public class demoControllerAuth {
 
 	
 	@Autowired
-    private IUsuarioRepository usuarioRepository;
+ 
+	private UsuariService usuarioService;
+	
 	
 
-	@PostMapping
-	public ResponseEntity<?> login(@RequestBody Usuario usuario) {
-	    Usuario usuarioEncontrado = usuarioRepository.findByEmailUsuario(usuario.getEmailUsuario());
-	    if (usuarioEncontrado != null && usuarioEncontrado.getConstrasenia().equals(usuario.getConstrasenia())) {
-	        return ResponseEntity.ok(usuarioEncontrado);
-	    } else {
-	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	 @PostMapping
+	    public ResponseEntity<?> autenticarUsuario(@RequestBody Usuario usuario) {
+	        String correo = usuario.getEmailUsuario();
+	        String contrasena = usuario.getConstrasenia();
+
+	        String tipoUsuario = usuarioService.autenticarUsuario(correo, contrasena);
+
+	        if (tipoUsuario != null) {
+	            return ResponseEntity.ok(tipoUsuario);
+	        } else {
+	            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("La autenticación ha fallado");
+	        }
 	    }
-	}
+
 }
